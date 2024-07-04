9c92a72018abf56f5b7d44e6043f6c35446bdd68
Sathish30122003 <139527435+Sathish30122003@users.noreply.github.com>
2024-07-04 12:38:50 +0530

    Time: 0 ms (100%), Space: 44 MB (47.78%) - LeetHub

--- 2980-check-if-bitwise-or-has-trailing-zeros/2980-check-if-bitwise-or-has-trailing-zeros.java
+++ 2980-check-if-bitwise-or-has-trailing-zeros/2980-check-if-bitwise-or-has-trailing-zeros.java
@@ -4,9 +4,9 @@ class Solution {
      for(int i=0;i<nums.length;i++){
         if(nums[i]%2==0)
         count++;
+        if(count>=2)
+        return true;
      }   
-     if(count>=2)
-     return true;
      return false;
     }
 }
