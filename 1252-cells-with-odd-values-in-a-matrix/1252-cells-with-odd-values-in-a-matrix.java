4fdf712272881ce93ceab45f99e9ddc7767f54ee
Sathish30122003 <139527435+Sathish30122003@users.noreply.github.com>
2024-07-05 16:00:38 +0530

    Time: 1 ms (92.4%), Space: 41.6 MB (36.21%) - LeetHub

---
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int oddCells(int m, int n, int[][] indices) {
+      int ans[][]=new int[m][n];
+      for(int i=0;i<indices.length;i++){
+            int row=indices[i][0];
+            int col=indices[i][1];
+            for(int j=0;j<n;j++){
+                ans[row][j]++;
+            }
+            for(int j=0;j<m;j++){
+                ans[j][col]++;
+            }
+      } 
+      int count=0;
+      for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(ans[i][j]%2==1)
+            count++;
+        }
+      } 
+      return count;
+    }
+}
