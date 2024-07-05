0289e2275bc28d3426c091b7fc9b38517f6209aa
Sathish30122003 <139527435+Sathish30122003@users.noreply.github.com>
2024-07-05 15:01:45 +0530

    Time: 308 ms (36.69%), Space: 55.1 MB (60.2%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String replaceWords(List<String> dictionary, String sentence) {
+      String arr[]=sentence.split(" ");
+      for(int i=0;i<dictionary.size();i++){
+        
+        for(int j=0;j<arr.length;j++){
+            if(arr[j].startsWith(dictionary.get(i))){
+                arr[j]=dictionary.get(i);
+            }
+        }
+      }
+      String temp="";
+      for(String s:arr)
+      temp+=s+" ";  
+      return temp.trim();
+    }
+}
