98cf465446eb7206789525c225b01e4f3d41dfeb
Sathish30122003 <139527435+Sathish30122003@users.noreply.github.com>
2024-07-05 17:47:36 +0530

    Time: 12 ms (13.76%), Space: 45.1 MB (5.55%) - LeetHub

--- 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.java
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.java
@@ -6,7 +6,7 @@ class Solution {
      int ans=0; 
      for(int i=grid[0].length-1;i>=0;i--){
         int max=Integer.MIN_VALUE;
-        System.out.println("0");
+        //System.out.println("0");
         for(int j=grid.length-1;j>=0;j--){
             max=Math.max(max,grid[j][i]);
         }
