994234172682e4b267ff215a44c36bf071c25d49
Sathish30122003 <139527435+Sathish30122003@users.noreply.github.com>
2024-07-06 13:25:56 +0530

    Time: 3 ms (95.09%), Space: 60 MB (71.03%) - LeetHub

---
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int garbageCollection(String[] garbage, int[] travel) {
+     int len=0;
+     for(int i=0;i<garbage.length;i++){
+        len+=garbage[i].length();
+     } 
+     int dis=0;
+     for(int i=1;i<travel.length;i++){
+        travel[i]+=travel[i-1];
+     }  
+      
+      int m=-1,p=-1,g=-1;
+      for(int i=garbage.length-1;i>=0;i--){
+        if(m==-1&&garbage[i].contains("M"))
+        m=i-1;
+        if(p==-1&&garbage[i].contains("P"))
+        p=i-1;
+        if(g==-1&&garbage[i].contains("G"))
+        g=i-1;
+      }
+
+        if(m==-1)
+         m=0;
+         else
+         m=travel[m];
+         if(p==-1)
+         p=0;
+         else
+         p=travel[p];
+         if(g==-1)
+         g=0;
+         else
+         g=travel[g];
+
+      return len+m+p+g;
+    }
+}
